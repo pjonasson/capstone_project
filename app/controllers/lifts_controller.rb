0fca05c3679c9eb522a2615bc6e1662f810eecc1
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-10-22 11:34:47 -0500

    Created index and show method for lifts

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,2 +1,11 @@
 class LiftsController < ApplicationController
+  def index
+    lifts = Lift.all
+    render json: lifts
+  end
+
+  def show
+    lift = Lift.find_by(id: params[:id])
+    render json: lift
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  get "/lifts" => "lifts#index"
+  get "/lifts/:id" => "lifts#show"
 end
