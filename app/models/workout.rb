f78f0fde562db78473952c6917c683cf75788667
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-11-04 13:43:09 -0500

    Added friendly_created_at for date and time. Changed time zone in config.

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,12 @@ class Workout < ApplicationRecord
   belongs_to :user
   has_many :lift_workouts
   has_many :lifts, through: :lift_workouts
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_time_created_at
+    created_at.strftime("%I:%M %p")
+  end
 end
