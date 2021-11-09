3c6418167d3bcc0383322cb72c61186487c2e5be
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-11-09 08:32:57 -0600

    Updated seeds file for chest and legs

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -2,6 +2,8 @@ class WorkoutSerializer < ActiveModel::Serializer
   attributes :id, :number_of_lifts, :lift_workouts, :created_at, :friendly_created_at, :friendly_time_created_at, :lifts
 
   belongs_to :user
-  has_many :lift_workouts
+  has_many :lift_workouts do
+    object.lift_workouts.order(:id => :asc)
+  end
   has_many :lifts
 end
